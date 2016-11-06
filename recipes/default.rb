1b301dbc01c80f434929bcea99b4e2966f30ac16
valbeat <kj1ktk@gmail.com>
2016-11-07 04:55:06 +0900

    CentOS7対応

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,17 @@
-mysql_service 'default' do
-  port '3306' 
-  version '5.6'
-  initial_root_password ''
-  action [:create, :start]
+remote_file "#{Chef::Config[:file_cache_path]}/mysql-community-release-el7-5.noarch.rpm" do
+  source 'http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm'
+  action :create
 end
 
-mysql_client 'default' do
-  action :create
+rpm_package 'mysql-community-release' do
+  source "#{Chef::Config[:file_cache_path]}/mysql-community-release-el7-5.noarch.rpm"
+  action :install
+end
+
+package 'mysql-server' do
+  action :install
+end
+
+service 'mysqld' do
+  action [:enable, :start]
 end
